7f16e9eaf7136ab65558fd0569be990483b9ebd8
Steven Scheidecker <displaypress@ubuntu.(none)>
2015-03-30 10:12:23 -0500

    Try Fugang patch

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -1294,7 +1294,7 @@ static int fec_enet_mii_probe(struct net_device *ndev)
 	fep->phy_dev = NULL;
 
 	/* check for attached phy */
-	for (phy_id = 0; (phy_id < PHY_MAX_ADDR); phy_id++) {
+	for (phy_id = 1; (phy_id < PHY_MAX_ADDR); phy_id++) {
 		if ((fep->mii_bus->phy_mask & (1 << phy_id)))
 			continue;
 		if (fep->mii_bus->phy_map[phy_id] == NULL)
@@ -1322,14 +1322,7 @@ static int fec_enet_mii_probe(struct net_device *ndev)
 	}
 
 	/* mask with MAC supported features */
-	if (id_entry->driver_data & FEC_QUIRK_HAS_GBIT) {
-		phy_dev->supported &= PHY_GBIT_FEATURES;
-#if !defined(CONFIG_M5272)
-		phy_dev->supported |= SUPPORTED_Pause;
-#endif
-	}
-	else
-		phy_dev->supported &= PHY_BASIC_FEATURES;
+	phy_dev->supported &= PHY_BASIC_FEATURES;
 
 	phy_dev->advertising = phy_dev->supported;
 
