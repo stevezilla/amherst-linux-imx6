e16e0eb680c61b6ff1d1b46c9685b0c081e6dfb9
root <root@ubuntu.(none)>
2016-07-12 23:37:24 -0500

    HDMI fix attempt

--- drivers/video/mxc/mxc_hdmi.c
+++ drivers/video/mxc/mxc_hdmi.c
@@ -2447,6 +2447,15 @@ static int mxc_hdmi_disp_init(struct mxc_dispdrv_handle *disp,
 	struct mxc_hdmi *hdmi = mxc_dispdrv_getdata(disp);
 	int irq = platform_get_irq(hdmi->pdev, 0);
 
+	mxc_hdmi_setup(hdmi, 0);
+        mxc_hdmi_enable_video_path(hdmi);
+        mxc_hdmi_cable_connected(hdmi);
+        #ifdef CONFIG_MXC_HDMI_CEC
+        mxc_hdmi_cec_handle(0x80);
+        #endif
+        hdmi_set_cable_state(1);
+
+
 	dev_dbg(&hdmi->pdev->dev, "%s\n", __func__);
 
 	/* Check hdmi disp init once */
